6e08c0bed1e8d0218ed586aa29135c22aa7cc7e2
Rocco Jiang <rocco.jiang5@gmail.com>
2021-03-19 18:26:00 +0000

    Add solution for 5235

---
+++ src/solutions/question5235/Point.java
@@ -0,0 +1,53 @@
+package solutions.question5235;
+
+public class Point {
+
+  private final double x;
+  private final double y;
+  private final double z;
+
+  public Point(double x, double y, double z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  public static Point origin() {
+    return new Point(0, 0, 0);
+  }
+
+  public double distanceFrom(Point other) {
+    double dx = x - other.x;
+    double dy = y - other.y;
+    double dz = z - other.z;
+
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+  }
+
+  public double magnitude() {
+    return distanceFrom(origin());
+  }
+
+  @Override
+  public String toString() {
+    return "(" + x + ", " + y + ", " + z + ")";
+  }
+
+  public static void main(String[] args) {
+    Point origin = origin();
+    Point point = new Point(3, 4, 5);
+
+    System.out.println(origin);
+    System.out.println(point);
+    System.out.println(point.magnitude());
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (!(other instanceof Point)) {
+      return false;
+    }
+    Point p = (Point) other;
+    return x == p.x && y == p.y && z == p.z;
+  }
+}
