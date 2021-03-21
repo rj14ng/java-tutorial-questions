72ac69d0f3e0891f29677e344914e99e8fe2909d
Rocco Jiang <rocco.jiang5@gmail.com>
2021-03-21 23:14:33 +0000

    Add solution for 236b

---
+++ src/solutions/question236b/Property.java
@@ -0,0 +1,11 @@
+package solutions.question236b;
+
+// Data fields merely have to be added to abstract Property superclass
+// Protected visibility so fields are visible to subclasses
+public abstract class Property {
+
+  protected int numOfBedrooms;
+  protected int numOfReceptionRooms;
+  protected int numOfBathrooms;
+  protected double squareFootage;
+}
